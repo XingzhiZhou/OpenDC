c3904b67708a51cedc42b17f84b03f860e43f37b
Jason Zhou <Xingzhi06Zhou@gmail.com>
2020-09-11 11:34:48 +0800

    fix one bug in cuda mode

--- example/mnist.py
+++ example/mnist.py
@@ -43,6 +43,7 @@ class CachedMNIST(Dataset):
             self._cache[index] = list(self.ds[index])
             if self.cuda:
                 self._cache[index][0] = self._cache[index][0].cuda(non_blocking=True)
+                self._cache[index][1] = torch.tensor(self._cache[index][1], dtype=torch.long)
                 self._cache[index][1] = self._cache[index][1].cuda(non_blocking=True)
         return self._cache[index]
 
